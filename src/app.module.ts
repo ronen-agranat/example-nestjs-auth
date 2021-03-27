f06ec2ffbd2df0d4a6112ad903eb50b25dfba9cc
Ronen Agranat <ronen.agranat@gmail.com>
2021-03-27 18:22:33 +0100

    Add users service, controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 import DatabaseConfig from './config/database.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(DatabaseConfig),
+    UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50, unique: true })
+  username: string;
+
+  // Do not share hashed password!
+  @Column({ length: 500 })
+  hashedPassword: string;
+
+  // Do not share refresh token!
+  @Column({ nullable: true })
+  public currentHashedRefreshToken?: string;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column()
+  isAdmin: boolean = false;
+
+  @CreateDateColumn()
+  updatedAt: Date;
+
+  @UpdateDateColumn()
+  createdAt: Date;
+}
